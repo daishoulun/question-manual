828959299d5e738d5c72eee94c0fc6cb31b7c647
apollo <33917670+skyeIce@users.noreply.github.com>
2021-08-17 14:28:10 +0800

    Update 文件流转url.js

--- 文件流转url.js
+++ 文件流转url.js
@@ -1,23 +1,34 @@
 function convertFileForDownload(fileData, fileName) {
-    // application/vnd.openxmlformats-officedocument.spreadsheetml.sheet这里表示xlsx类型
-    let blob = new Blob([fileData], {
-      type:
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8",
-    });
-    console.log(blob)
-    var downloadElement = document.createElement("a");
-    var href = window.URL.createObjectURL(blob); // 创建下载的链接
-    downloadElement.href = href;
-    downloadElement.download = fileName; // 下载后文件名
-    document.body.appendChild(downloadElement);
-    downloadElement.click(); // 点击下载
-    document.body.removeChild(downloadElement); // 下载完成移除元素
-    window.URL.revokeObjectURL(href); // 释放掉blob对象
+  // application/vnd.openxmlformats-officedocument.spreadsheetml.sheet这里表示xlsx类型
+  let blob = new Blob([fileData], {
+    type:
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8",
+  });
+  console.log(blob)
+  var downloadElement = document.createElement("a");
+  var href = window.URL.createObjectURL(blob); // 创建下载的链接
+  downloadElement.href = href;
+  downloadElement.download = fileName; // 下载后文件名
+  document.body.appendChild(downloadElement);
+  downloadElement.click(); // 点击下载
+  document.body.removeChild(downloadElement); // 下载完成移除元素
+  window.URL.revokeObjectURL(href); // 释放掉blob对象
+}
+function exportFile() {
+  exportExcel().then(res => {
+    let filename = res.headers['content-disposition'] // 获取文件名
+        filename = filename.split(";")[1].split("filename=")[1];
+    this.convertFileForDownload(res.data, filename)
+  })
 }
 
-
-
-
+function exportExcel() {
+  return request({
+    url: '/resourceLoad/downExcel',
+    method: 'post',
+    responseType: 'blob' // 不加responseType，下载下来的文件打不开
+  })
+}
 
 
 
