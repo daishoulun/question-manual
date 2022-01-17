d1f9aaf8e84b38b15a6a83a1a2c90955f8a27ba9
daishoulun <33917670+daishoulun@users.noreply.github.com>
2022-01-17 16:36:53 +0800

    Create aboutIE.js

---
+++ aboutIE.js
@@ -0,0 +1,18 @@
+function downloadFile(res) {
+    const blob = new Blob([res], {type: 'application/vnd.openxmlformats-officedocument.presentationml.presentation'})
+    const fileName = ''
+    if (window.navigator && window.navigator.msSaveBlob){ // 兼容IE
+      window.navigator.msSaveBlob(blob, fileName);
+      return;
+    }
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    link.download = fileName
+    link.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(link);
+    console.log(link.download)
+    link.click();
+    document.body.removeChild(link);
+}
+
+// IE url不支持中文，会报错
